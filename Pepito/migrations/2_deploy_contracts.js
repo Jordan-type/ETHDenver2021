2c355b24a7dedf9729a11015716ff1fda133f144
rajanpaulsarella <s.rajanpaul@gmail.com>
2021-02-07 16:34:33 -0600

    Working on local CELO blockchain

--- Pepito/migrations/2_deploy_contracts.js
+++ Pepito/migrations/2_deploy_contracts.js
@@ -1,10 +1,27 @@
 const PepitoDisguise = artifacts.require("PepitoDisguise");
 const Pepito = artifacts.require("Pepito");
 
-module.exports = function (deployer, network, accounts) {
-    const userAddress = accounts[1];              // my Metamask account with Kovan ETH
-    deployer.deploy(PepitoDisguise, userAddress); /* pepitoDisguise constructor expects ownerAddress
-        but since all disguises are dynamically deployed by Pepito, this deployment here is rather academic */
-  deployer.deploy(Pepito);
-  };
+const path = require("path");
 
+const Web3 = require('web3')
+const ContractKit = require('@celo/contractkit')
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org')
+const kit = ContractKit.newKitFromWeb3(web3)
+const getAccount = require('../getAccount').getAccount
+
+async function awaitWrapper(){
+    let account_details = await getAccount()
+    //console.log(account_details)
+    let account = account_details.address
+    //console.log(account)
+}
+awaitWrapper()
+//console.log("Here" + userAddress);
+
+module.exports = function (deployer, network, account) {
+  const userAddress = account;
+  //console.log("Here" + userAddress);
+  deployer.deploy(Pepito);
+  deployer.deploy(PepitoDisguise, '0x04b903951CdC7c0C1A90A00c7bb6Ce1903836Ef5');
+  //deployer.deploy(PepitoDisguise, userAddress);
+};
